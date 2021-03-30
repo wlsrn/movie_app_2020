45f2af880722338d660271565a79253516e4d4a5
Kwon_jingu <wlsrndmswls1@naver.com>
2021-03-30 23:20:58 +0900

    3/30 study

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
-function App() {
-  return <div>Hello</div>
+function Food({ name }) {
+  return <h1>I like {name}</h1>;
 }
-//?뭐지?dfadfsdkf
+
+const foodILike = [
+  {
+    name : "kimchi"
+  },
+  {
+    name : "삼겹살"
+  },
+  {
+    name : "김밥"
+  },
+  {
+    name : "족발"
+  }
+]
+
+
+function App() {
+  return (
+  <div>
+    {foodILike.map(dish => <Food name = {dish.name} picture = {dish.image} />)}
+  </div>
+  //2.2     9 : 19
+  
+  );
+}
+
 export default App;
