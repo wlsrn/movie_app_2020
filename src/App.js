dbbd6856c11eeb6ecb54ff1af176b922e43995f8
Kwon_jingu <wlsrndmswls1@naver.com>
2021-03-31 23:17:23 +0900

    3/31 Last study in March

--- src/App.js
+++ src/App.js
@@ -1,59 +1,65 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
 
-function Food({ name, picturem, rating }) {
+const foodILike = [
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 4.9
+  },
+  {
+    id: 3,
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 4.8
+  },
+  {
+    id: 4,
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 5.5
+  },
+  {
+    id: 5,
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4.7
+  }
+];
+
+function Food({ name, picture, rating }) {
   return (
     <div>
-      <h2>I kile {name}</h2>
-      <h4>{rating}/5 </h4>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
       <img src={picture} alt={name} />
     </div>
   );
 }
-//2.4  3:43
-Food.PropTypes={
-
-}
-
-const foodILike = [
-  {
-    id : 1,
-    name : "kimchi",
-    rating: 5
-  },
-  {
-    id : 2,
-    name : "삼겹살",
-    rating:4.9
-  },
-  {
-    id : 3,
-    name : "김밥",
-    rating:5.5
-  },
-  {
-    id : 4,
-    name : "족발",
-    rating:4.7
-  }
-]
-
+//2.4    5 : 44
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
 
 function App() {
   return (
-  <div>
-   {foodILike.map(dish => (
-     <Food 
-      key={dish.id}
-      name={dish.name}
-      picture={dish.imge}
-      rating={dish.rating}
-       />
-   ))}
-  </div>
-
-  
+    <div>
+      {foodILike.map(dish => (
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
+      ))}
+    </div>
   );
 }
-
 export default App;
