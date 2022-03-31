dfaf7324b22588e984c7504ae86001b4e38af06f
Kwon_Jingu <wlsrndmswls1@naver.com>
2022-03-31 09:16:31 +0900

    fix

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,16 +1,17 @@
 import React from "react";
+import "./About.css";
 
 function About(props) {
-    console.log(props);
-    return( 
-        <div>
-            <span>
-                About this page:
-                I built it because I love movies.
-            </span>
-            <span>-George Orwell,</span>
-        </div>
-    );
+  console.log(props);
+  return (
+    <div className="about__container">
+      <span>
+        “Freedom is the freedom to say that two plus two make four. If that is
+        granted, all else follows.”
+      </span>
+      <span>− George Orwell, 1984</span>
+    </div>
+  );
 }
 
 export default About;
